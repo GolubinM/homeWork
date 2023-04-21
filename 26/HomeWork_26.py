ddd94ce11a1037eab1703b25c59155ed787dd22e
Mihail Golubin <golm2020@gmail.com>
2023-04-21 21:35:13 +0300

    t26.1.2 ready

--- 26/HomeWork_26.py
+++ 26/HomeWork_26.py
@@ -36,25 +36,34 @@ class RcTriangle(Figure):
         return self.other[0] * self.other[1] / 2
 
 
+class Trapezoid(Figure):
+    def __init__(self, a, b, h):
+        super().__init__(a, b, h)
+
+    def area(self):
+        return (self.other[0] + self.other[1]) * self.other[2] / 2
+
+
 rc1 = Rectangle(5, 10)
 cr1 = Circle(5)
 rcTr1 = RcTriangle(5, 10)
-print(rc1.area(), cr1.area(), rcTr1.area())
-print(rc1, cr1, rcTr1)
+tr1 = Trapezoid(10, 5, 7)
+print(rc1.area(), cr1.area(), rcTr1.area(), tr1.area(), sep="; ")
+print(rc1, cr1, rcTr1, tr1, sep="\n")
 
 print('=Задание 2=Переопределить методы классов Задания 1=======================================================')
 
 
-class Figure:
-    def __init__(self, side1, side2=0):
-        self.side1 = side1
-        self.side2 = side2
+class Figure(ABC):
+    def __init__(self, *other):
+        self.other = other
 
+    @abstractmethod
     def area(self):
-        return self.side1 * self.side2
+        pass
 
     def __str__(self):
-        return f"Фигура класса {self.__class__.__name__} площадь: {self.area()}."
+        return f"Фигура класса {self.__class__.__name__} площадь: {self.area()}"
 
     def __int__(self):
         return self.area()
@@ -65,7 +74,7 @@ class Rectangle(Figure):
         super().__init__(side1, side2)
 
     def area(self):
-        return self.side1 * self.side2
+        return self.other[1] * self.other[0]
 
 
 class Circle(Figure):
@@ -73,7 +82,7 @@ class Circle(Figure):
         super().__init__(r)
 
     def area(self):
-        return self.side1 ** 2 * 3.14
+        return self.other[0] ** 2 * 3.14
 
 
 class RcTriangle(Figure):
@@ -81,13 +90,23 @@ class RcTriangle(Figure):
         super().__init__(side1, side2)
 
     def area(self):
-        return self.side1 * self.side2 / 2
+        return self.other[0] * self.other[1] / 2
+
+
+class Trapezoid(Figure):
+    def __init__(self, a, b, h):
+        super().__init__(a, b, h)
+
+    def area(self):
+        return (self.other[0] + self.other[1]) * self.other[2] / 2
 
 
 rc1 = Rectangle(5, 10)
 cr1 = Circle(5)
 rcTr1 = RcTriangle(5, 10)
-print(rc1.area(), cr1.area(), rcTr1.area(), sep="; ")
+tr1 = Trapezoid(10, 5, 7)
+print(rc1, cr1, rcTr1, tr1, sep="\n")
+print(rc1.__int__(), cr1.__int__(), rcTr1.__int__(), tr1.__int__(), sep="; ")
 
-print(rc1, cr1, rcTr1,sep="\n")
-print(rc1.__int__(), cr1.__int__(), rcTr1.__int__(), sep="; ")
+print('=Задание 3=Shape=======================================================')
+
